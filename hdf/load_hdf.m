93459622f49a579503ccb5af96d2f71fa41b5019
tcaswell <tcaswell@68ef01f2-2417-43ed-be7d-edefb07f4695>
2010-04-01 19:32:06 +0000

    changed to work with new naming scheme
    git-svn-id: svn://localhost/tcaswell/trunk/matlab@303 68ef01f2-2417-43ed-be7d-edefb07f4695

--- hdf/load_hdf.m
+++ hdf/load_hdf.m
@@ -1,23 +1,30 @@
-function data = load_hdf(fname)
+function data = load_hdf(fname,comp_num)
 % LOAD_HDF - loads an hdf file into the form expected by all of the current matlab code
 %   
     hinfo = hdf5info(fname);
     data = zeros(1e6,10);
     count = 0;
+    post_fix = sprintf('_%07i',comp_num);
     for j = 1:length(hinfo.GroupHierarchy.Groups)
         tmp_group = hinfo.GroupHierarchy.Groups(j);
+        if(strcmp(tmp_group.Name,'/parameters'))
+            
+            break
+        end
+        disp(tmp_group.Name)
         part_count =tmp_group.Datasets(1).Dims ;
         tmp = zeros(part_count,10);
         
+
         
-        tmp(:,2)=hdf5read(tmp_group.Filename,[tmp_group.Name '/x']);
-        tmp(:,3)=hdf5read(tmp_group.Filename,[tmp_group.Name '/y']);
-        tmp(:,4)=hdf5read(tmp_group.Filename,[tmp_group.Name '/x_shift']);
-        tmp(:,5)=hdf5read(tmp_group.Filename,[tmp_group.Name '/y_shift']);
-        tmp(:,6)=hdf5read(tmp_group.Filename,[tmp_group.Name '/intensity']);
-        tmp(:,7)=hdf5read(tmp_group.Filename,[tmp_group.Name '/R2']);
-        tmp(:,8)=hdf5read(tmp_group.Filename,[tmp_group.Name '/multiplicity']);
-        tmp(:,9)=hdf5read(tmp_group.Filename,[tmp_group.Name '/eccentricity']);
+        tmp(:,2)=hdf5read(tmp_group.Filename,[tmp_group.Name '/x' post_fix ]);
+        tmp(:,3)=hdf5read(tmp_group.Filename,[tmp_group.Name '/y' post_fix ]);
+        tmp(:,4)=hdf5read(tmp_group.Filename,[tmp_group.Name '/x_shift' post_fix ]);
+        tmp(:,5)=hdf5read(tmp_group.Filename,[tmp_group.Name '/y_shift' post_fix ]);
+        tmp(:,6)=hdf5read(tmp_group.Filename,[tmp_group.Name '/intensity' post_fix ]);
+        tmp(:,7)=hdf5read(tmp_group.Filename,[tmp_group.Name '/R2' post_fix ]);
+        tmp(:,8)=hdf5read(tmp_group.Filename,[tmp_group.Name '/multiplicity' post_fix ]);
+        tmp(:,9)=hdf5read(tmp_group.Filename,[tmp_group.Name '/eccentricity' post_fix ]);
         tmp(:,end) = parse_frame_name(tmp_group.Name);
         
         if(count + part_count > size(data,1))
