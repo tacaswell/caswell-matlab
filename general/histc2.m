f18e5375b0afc15c95c6a0885cb2dcc2f940efc2
Thomas A Caswell <tcaswell@uchicago.edu>
2011-01-26 17:52:42 -0600

    fixed copy and paste error

--- general/histc2.m
+++ general/histc2.m
@@ -3,7 +3,7 @@ function out = histc2(data,x_range,y_range)
 %   the ranges are of the form [min max steps]
     
     data = data(data(:,1)>x_range(1)&data(:,1)<x_range(2)&...
-                data(:,2)>x_range(1)&data(:,2)<y_range(2),:);
+                data(:,2)>y_range(1)&data(:,2)<y_range(2),:);
     
     x_bin=floor(((data(:,1)-x_range(1))/(x_range(2)-x_range(1)))*x_range(3))+1;
     y_bin=floor(((data(:,2)-y_range(1))/(y_range(2)-y_range(1)))*y_range(3))+1;
