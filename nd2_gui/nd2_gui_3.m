4106e12f66c0a4e0ad17f7813ab0a54358d0d37a
Thomas A Caswell <tcaswell@uchicago.edu>
2011-01-26 18:21:12 -0600

    switched some scatter plots to 2D histograms.  They are much more informative now.
    Left some more random numbers scattered in code

--- nd2_gui/nd2_gui_3.m
+++ nd2_gui/nd2_gui_3.m
@@ -11,13 +11,13 @@ function circ_handles = nd2_gui_3(filename)
     p_rad = 5;           %size of particles to be looking for
     image_index = 1;
     series_index = 0;
-    hwhm = 1.4;
-    d_rad = 3;
-    mask_rad = 4;
+    hwhm = 1.2;
+    d_rad = 4;
+    mask_rad = 5;
 
-    shift_cut = 1.5;
-    rg_cut = 7.5;
-    e_cut = .6;
+    shift_cut = 1;
+    rg_cut = 12;
+    e_cut = .5;
 
     disp_mode = 1;
     
@@ -29,7 +29,7 @@ function circ_handles = nd2_gui_3(filename)
     r = loci.formats.ChannelFiller();
    
     %%uncomment to stich files
-% $$$     r = loci.formats.FileStitcher(r);
+    r = loci.formats.FileStitcher(r);
 
     r.setId(filename);
 
@@ -96,14 +96,14 @@ image_index_disp = uicontrol(fh,'style','text','string',image_index,...
 colormap(gray)
 hold on;
 scatter_h2 = scatter([],[],'g*');
-scatter_h = scatter([],[],'r.');
-
 scatter_h3 = scatter([],[],'b.');
+scatter_h = scatter([],[],'rs');
 %%generate exctra graphs
 
 %e v rg
 fh2 = figure;
-sc2 = scatter([],[],'rx');
+sc2 = imagesc([0,1],[0,25],zeros(125,250))
+set(gca,'ydir','normal')
 hold on
 lh2 = plot([0 0],'--k');
 lv2 = plot([0 0],'--k');
@@ -113,7 +113,9 @@ title('rg v e');
 
 %e vs shift
 fh3 = figure;
-sc3 = scatter([],[],'rx');
+sc3 = imagesc([0,1],[0,3],zeros(125,125))
+set(gca,'ydir','normal')
+colormap(jet)
 hold on
 lh3 = plot([0 0],'--k');
 lv3 = plot([0 0],'--k');
@@ -125,6 +127,7 @@ title('e v shift');
 fh4 = figure;
 sc4 = scatter([],[],'rx');
 hold on
+
 %lh4 = plot([0 0],'--k');
 lv4 = plot([0 0],'--k');
 ylabel('I')
@@ -397,34 +400,12 @@ function rebuild_buffer
 % $$$     buffer_current = find(buffer_index==buffer_center);
     
     for j = 1:buffer_length
-        img = extract_image(r,buffer_index(j));
-        
-        tic
-            [pks b_passed centers] = matlab_process(img,[p_rad,hwhm,d_rad, ...
-                            mask_rad,threshold,0],top_cut);
-            
-        toc
-        clear matlab_process
-    
-        pks = pks';
-
-        % convert Java BufferedImage to MATLAB image
-        
-        if disp_mode ==0
-            buffer_pix(:,:,j) = b_passed + 0*centers;
-            buffer_pix2(:,:,j) = img;%.*(ones(size(centers))-centers);
-        else
-            buffer_pix(:,:,j) = img;%.*(ones(size(centers))-centers);
-            buffer_pix2(:,:,j) = b_passed + 0*centers;
-        end
-        buffer_pos{j} = pks(:,[1 3 2 5 4 6:9]);
-       
+        fill_buffer(j);
         
     end
     
 
 end
-
 
 function update_buffer
 %    while update_running
@@ -457,7 +438,10 @@ function update_buffer
     elseif buffer_center+buffer_frames<numImages && buffer_center-buffer_frames<1
             buffer_index_tmp = 1:buffer_length
     elseif buffer_center+buffer_frames>numImages && buffer_center-buffer_frames>0
-            buffer_index_tmp = (numImages-buffer_length):buffer_length
+            buffer_index_tmp = (numImages-buffer_length): ...
+                buffer_length
+    else
+        dilspay('wtf, did not hit anything')
     end
 
 
@@ -465,32 +449,37 @@ function update_buffer
     %figure out what has changed and change it
     change_index = find(buffer_index ~=buffer_index_tmp);
     for j = change_index
-        img = extract_image(r,buffer_index(j));
-        
-        tic
-            % convert Java BufferedImage to MATLAB image
-            [pks b_passed centers] = matlab_process(img,[p_rad,hwhm,d_rad,mask_rad, ...
-                                threshold,0],top_cut);
-        toc
-        clear matlab_process
-        pks = pks';
-
-        
-        if disp_mode==0
-            buffer_pix(:,:,j) = b_passed + 0*centers;
-            buffer_pix2(:,:,j) = img;%.*(ones(size(centers))-centers);
-        else
-            buffer_pix(:,:,j) = img;%.*(ones(size(centers))-centers);
-            buffer_pix2(:,:,j) = b_passed + 0*centers;
-        end
-        buffer_pos{j} = pks(:,[1 3 2 5 4 6:9]);
-
- 
-        
+        fill_buffer(j)
     end
     set(bc,'string',num2str(buffer_center))
     buffer_index = buffer_index_tmp
     buffer_current = buffer_current_tmp;
+end
+
+
+function fill_buffer(j)
+    img = extract_image(r,buffer_index(j));
+    
+    tic
+        [pks b_passed centers] = matlab_process(img,[p_rad,hwhm,d_rad, ...
+                            mask_rad,threshold,top_cut]);
+        
+    toc
+    clear matlab_process
+    
+    pks = pks';
+
+    % convert Java BufferedImage to MATLAB image
+    
+    if disp_mode ==0
+        buffer_pix(:,:,j) = b_passed + 0*centers;
+        buffer_pix2(:,:,j) = img;%.*(ones(size(centers))-centers);
+    else
+        buffer_pix(:,:,j) = img;%.*(ones(size(centers))-centers);
+        buffer_pix2(:,:,j) = b_passed + 0*centers;
+    end
+    buffer_pos{j} = pks(:,[1 3 2 5 4 6:9]);
+    
 end
 
 
@@ -517,16 +506,19 @@ function update_display
 
     
     tmp = buffer_pos{buffer_current};
-    
-    set(sc2,'xdata',tmp(:,9));
-    set(sc2,'ydata',tmp(:,7));
+    tmp_hist2 = histc2([tmp(:,9),tmp(:,7)],[0,1,125],[0,25,250]);
+    set(sc2,'cdata',tmp_hist2);
     set(lh2,'xdata',get(get(fh2,'currentaxes'),'xlim'))
     set(lh2,'ydata',[rg_cut rg_cut]);
     set(lv2,'ydata',get(get(fh2,'currentaxes'),'ylim'))
     set(lv2,'xdata',[e_cut e_cut]);
     
-    set(sc3,'xdata',tmp(:,9));
-    set(sc3,'ydata',sqrt((sum(tmp(:,[4 5]).^2,2))));
+    tmp_hist2 = histc2([tmp(:,9),sqrt((sum(tmp(:,[4 5]).^2,2)))],[0,1,125],[0,3,125]);
+    
+    set(sc3,'cdata',tmp_hist2);
+    
+    
+    
     set(lh3,'xdata',get(get(fh3,'currentaxes'),'xlim'))
     set(lh3,'ydata',[shift_cut shift_cut]);
     set(lv3,'ydata',get(get(fh3,'currentaxes'),'ylim'))
